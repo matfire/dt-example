6349e0e095273bbc5f20eee56a8c018550cbcaa0
Matteo Gassend <matteo.gassend@agiltech.fr>
2023-11-15 16:15:50 +0100

    add: stimulus controller for datatable

---
+++ assets/controllers/datatable_controller.js
@@ -0,0 +1,40 @@
+import { Controller } from "@hotwired/stimulus";
+import $ from 'jquery'
+import '../styles/datatables.css'
+import jszip from 'jszip';
+import pdfmake from 'pdfmake';
+import DataTable from 'datatables.net-dt'
+import 'datatables.net-buttons-dt';
+import 'datatables.net-buttons/js/buttons.colVis.mjs';
+import 'datatables.net-buttons/js/buttons.html5.mjs';
+import 'datatables.net-buttons/js/buttons.print.mjs';
+import 'datatables.net-fixedcolumns-dt';
+import 'datatables.net-fixedheader-dt';
+/* stimulusFetch: 'lazy' */
+export default class extends Controller {
+    static targets = ["table"]
+    static values = {
+        buttons: Array
+    }
+
+    connect() {
+        new DataTable(this.tableTarget, {
+            dom:"Blfrtip",
+            buttons: ["colvis", ...this.buttonsValue],
+            language: {
+                emptyTable:"Pas de données pour cette table",
+                info: "Affiche les éléments _START_ à _END_ sur _TOTAL_",
+                infoEmpty: "Affiche 0 éléments sur 0",
+                search: "Chercher:",
+                zeroRecords: "Aucun résultat corréspond à cette recherche",
+                lengthMenu: "Affiche _MENU_ éléments",
+                paginate: {
+                    first:"Début",
+                    last:"Fin",
+                    next:"Suivant",
+                    previous:"Précédant"
+                }
+            }
+        })
+    }
+}
