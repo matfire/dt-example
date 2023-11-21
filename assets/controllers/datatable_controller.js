c27dd507250db148adce5d1589a45a095fcb5d4b
Matteo Gassend <matteo.gassend@agiltech.fr>
2023-11-21 12:31:25 +0100

    rediying example

--- assets/controllers/datatable_controller.js
+++ assets/controllers/datatable_controller.js
@@ -1,28 +1,32 @@
 import { Controller } from "@hotwired/stimulus";
 import '../styles/datatables.css'
-import $ from 'jquery'
+import '../styles/buttons.bootstrap5.css'
 import jszip from 'jszip';
 import pdfmake from 'pdfmake';
+import $ from 'jquery'
 import DataTable from 'datatables.net-dt'
 import 'datatables.net-bs5'
 import 'datatables.net-buttons-dt';
+import 'datatables.net-buttons-bs5'
 import 'datatables.net-buttons/js/buttons.colVis.mjs';
-//TODO fix pdf import
-//import 'datatables.net-buttons/js/buttons.html5.mjs';
+import 'datatables.net-buttons/js/buttons.html5.mjs';
 import 'datatables.net-buttons/js/buttons.print.mjs';
 import 'datatables.net-fixedcolumns-dt';
 import 'datatables.net-fixedheader-dt';
 
+
+/* stimulusFetch: 'lazy' */
 export default class extends Controller {
     static targets = ["table"]
     static values = {
-        buttons: Array
+        buttons: Array,
+        disabled: Array
     }
 
     connect() {
         new DataTable(this.tableTarget, {
-            dom:"Blfrtip",
             buttons: ["colvis", ...this.buttonsValue],
+            columnDefs: [{searchable:false, targets: this.disabledValue}],
             language: {
                 emptyTable:"Pas de données pour cette table",
                 info: "Affiche les éléments _START_ à _END_ sur _TOTAL_",
--- src/Controller/ResourcesController.php
+++ src/Controller/ResourcesController.php
@@ -2,10 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Lvb;
-use App\Repository\BoxRepository;
-use App\Repository\LvbRepository;
-use App\Repository\VehicleRepository;
 use DateTime;
 use DateTimeZone;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,25 +11,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/resources')]
 class ResourcesController extends AbstractController {
     #[Route('/box')]
-    public function boxResources(BoxRepository $boxRepository, LvbRepository $lvbRepository) {
+    public function boxResources() {
         $date = new DateTime("now", new DateTimeZone("GMT"));
-        $lvbs = $lvbRepository->findActiveOnDate(425, $date->getTimestamp());
-        $lvbIds = array_map(function (Lvb $el) {
-            return $el->getBOXID();
-        }, $lvbs);
-        $boxes = $boxRepository->findByBOXIDList($lvbIds);
-        return $this->render("resources/box.html.twig", ["boxes" => $boxes, "factor" => 2, "currentTs" => $date->getTimestamp()]);
+
+        return $this->render("resources/box.html.twig", ["boxes" => [], "factor" => 2, "currentTs" => $date->getTimestamp()]);
     }
 
-    #[Route('/box/{boxId}')]
-    public function boxDetailResources(int $boxId, BoxRepository $boxRepository, LvbRepository $lvbRepository, VehicleRepository $vehicleRepository) {
-
-        $box = $boxRepository->findByBoxId($boxId);
-        $lvbs = $lvbRepository->findByBOXID($box[0]->getBOXID(), (new DateTime("now", new DateTimeZone("GMT")))->getTimestamp());
-        usort($lvbs, function (Lvb $a, Lvb $b) {
-            return $b->getLVBID() - $a->getLVBID();
-        });
-        $vehicle = $vehicleRepository->findOneBy(["VHC_ID" => $lvbs[0]->getVHCID()]);
-        return $this->render("resources/details.box.html.twig", ["box" => $box[0], "vehicle" => $vehicle]);
-    }
 }
